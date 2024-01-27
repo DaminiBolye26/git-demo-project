0fa7866b813ef5fee787781a463d321399550c4c
DaminiBolye26 <2023.damini.bolye@ves.ac.in>
2024-01-27 14:47:58 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Input: two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+result = num1 + num2
+
+# Display the result
+print("The sum of {} and {} is: {}".format(num1, num2, result))
