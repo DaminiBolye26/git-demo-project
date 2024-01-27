991cf061b60703f2c8ea2bd71ef09ae935f27ab4
DaminiBolye26 <2023.damini.bolye@ves.ac.in>
2024-01-27 15:18:29 +0530

    commiting changes in diff.py

---
+++ diff.py
@@ -0,0 +1,9 @@
+# Input: two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the difference
+difference = num1 - num2
+
+# Display the result
+print("The difference between {} and {} is: {}".format(num1, num2, difference))
