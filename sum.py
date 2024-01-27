f20e2e747222c3c04b71eaac1d17234374cc1f60
DaminiBolye26 <2023.damini.bolye@ves.ac.in>
2024-01-27 16:07:56 +0530

    commiting sum.py

--- sum.py
+++ sum.py
@@ -1,9 +1,10 @@
 # Input: two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the second number: "))
 
 # Add the numbers
-result = num1 + num2
+result = num1 + num2 + num3
 
 # Display the result
-print("The sum of {} and {} is: {}".format(num1, num2, result))
+print("The sub of {} and {} is: {}".format(num1, num2, num3, result))
